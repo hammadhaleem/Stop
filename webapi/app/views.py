d0f91f8253bcd57d33c2c8504ae572c92943f418
hammadhaleem <hammadhaleem@gmail.com>
2014-12-03 19:18:04 +0800

    Advanced

--- webapi/app/views.py
+++ webapi/app/views.py
@@ -88,3 +88,49 @@ def convert_file(filename):
     conf=api.MeanTextConf()
     return jsonify({'output' : str(text)})
 #return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
+import cv2.cv as cv
+import tesseract
+import re
+from string import punctuation
+
+def header(mstr,stars):
+    print "%s %s %s"%(stars,mstr,stars)
+
+def countWords(strs):
+    r = re.compile(r'[{}]'.format(punctuation))
+    new_strs = r.sub(' ',strs)
+    return len(new_strs.split())
+
+def countWords2(strs):
+    strs=strs.strip()
+    words=re.split(r'[^0-9A-Za-z]+',strs)
+    #print words
+    return len(words)
+
+@app.route('/converts/<filename>')
+def conver_file_advance(filename):
+	path = str('/home/engineer/htdocs/stop/webapi/uploads/'+filename).lower()
+	api = tesseract.TessBaseAPI()
+	api.Init(".","eng",tesseract.OEM_DEFAULT)
+	api.SetPageSegMode(tesseract.PSM_AUTO)
+	image=cv.LoadImage(path, cv.CV_LOAD_IMAGE_GRAYSCALE)
+	tesseract.SetCvImage(image,api)
+	text=api.GetUTF8Text()
+	conf=api.MeanTextConf()
+	print text,len(text)
+	print "Cofidence Level: %d %%"%conf
+	print "Confidences of All words"
+	header("Method 1","*"*10)
+	confOfText=api.AllWordConfidences()
+
+	print confOfText
+	print "Number of Words:"
+	print "counted by tesseract: %d"%len(confOfText) 
+	print "counted by me: %d[%d]"%(countWords(text), countWords2(text))
+	if len(confOfText)!=countWords(text):
+	        print "Why the words counted by tesseract are different from mine!!!!"
+	header("Method 2","*"*10)
+	confs=tesseract.AllWordConfidences(api)
+	print confs, len(confs)
+	return str(conf)
