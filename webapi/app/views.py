7965cfd75ca7aea5acba1169f6b08064b1668145
hammadhaleem <hammadhaleem@gmail.com>
2014-12-15 03:16:25 +0800

    barcode

--- webapi/app/views.py
+++ webapi/app/views.py
@@ -82,7 +82,8 @@ def upload():
         return jsonify({
         'file' : url_for('uploaded_file',filename=filename),
         'name' : filename,
-        'ocr'  : url_for('convert_file',filename=filename)
+        'ocr'  : url_for('convert_file',filename=filename),
+        'barcode' : url_for('bar_Code',filename=filename)
         })
     else:
     	return(str("Error!!"))
@@ -92,6 +93,47 @@ def upload():
 def uploaded_file(filename):
     return send_from_directory('/home/engineer/htdocs/stop/webapi/uploads',filename)
     #return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+@app.route('/barcode/<filename>')
+@app.route('/barcode/<filename>')
+def bar_Code(filename):
+  path = str('/home/engineer/htdocs/stop/webapi/uploads/'+filename).lower()
+  image = cv2.imread(path)
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  gradX = cv2.Sobel(gray, ddepth = cv2.cv.CV_32F, dx = 1, dy = 0, ksize = -1)
+  gradY = cv2.Sobel(gray, ddepth = cv2.cv.CV_32F, dx = 0, dy = 1, ksize = -1)
+
+  # subtract the y-gradient from the x-gradient
+  gradient = cv2.subtract(gradX, gradY)
+  gradient = cv2.convertScaleAbs(gradient)
+
+  # blur and threshold the image
+  blurred = cv2.blur(gradient, (9, 9))
+  (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+
+  # construct a closing kernel and apply it to the thresholded image
+  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+  closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+  # perform a series of erosions and dilations
+  closed = cv2.erode(closed, None, iterations = 4)
+  closed = cv2.dilate(closed, None, iterations = 4)
+
+  # find the contours in the thresholded image, then sort the contours
+  # by their area, keeping only the largest one
+  (cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+  c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+
+  # compute the rotated bounding box of the largest contour
+  rect = cv2.minAreaRect(c)
+  box = np.int0(cv2.cv.BoxPoints(rect))
+
+  # draw a bounding box arounded the detected barcode and display the
+  # image
+  cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+  cv2.imwrite('image.png',image[box])
+
+  return image
 
 @app.route('/convert/<filename>/')
 @app.route('/convert/<filename>')
