36ed6575c9a9133a6d43e52760a1f38784bb61bf
hammadhaleem <hammadhaleem@gmail.com>
2014-12-15 20:21:27 +0800

    eerro

--- webapi/app/views.py
+++ webapi/app/views.py
@@ -253,13 +253,14 @@ def register(username,email,phone_number,password):
 @app.route('/get/<userid>')
 @app.route('/get/<userid>/')
 def get_added_good(userid):
-  lis = []
+  lis = {}
   if userid is None: 
     return jsonify({'status' : 'error'})
   goods = Goods.query.filter_by(userid = userid).all()
+  count = 0 
   for i in goods : 
-    lis.append(i.getdata())
-  return jsonify({"objects" : lis})
+    lis[count] = i.getdata()
+  return jsonify(lis)
 
 @app.route('/AddProduct/<Price>/<Pictureid>/<Longitude>/<Latitude>/<Goodsname>/<Goodsdescription>/<address>/<userid>')
 @app.route('/AddProduct/<Price>/<Pictureid>/<Longitude>/<Latitude>/<Goodsname>/<Goodsdescription>/<address>/<userid>')
