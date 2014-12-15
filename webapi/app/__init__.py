57440564039a79f0be72cb42bb644edd4f021a60
hammadhaleem <hammadhaleem@gmail.com>
2014-12-15 12:47:47 +0800

    barcode

--- webapi/app/__init__.py
+++ webapi/app/__init__.py
@@ -9,9 +9,7 @@ from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, \
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-lm = LoginManager()
-lm.init_app(app)
-lm.login_view = 'login'
+db.init_app(app) 
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 
